67dc58cc7857075df2465e826cf7ac5fc820cd91
José Rodolfo Salas Montañez <jsalas@FACTURACTIVA.COM>
2019-09-10 14:08:33 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+import './styles.scss';
+
+const Index = () => {
+  return <div><App/></div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
